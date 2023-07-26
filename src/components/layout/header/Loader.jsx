3f7c62a127bbd187940509f4a1e490af3946cce5
Raj <rajlilhare200@gmail.com>
2023-07-26 09:32:51 -0700

    add loader

--- src/components/layout/header/Loader.jsx
+++ src/components/layout/header/Loader.jsx
@@ -1,11 +1,17 @@
 import {  VStack } from '@chakra-ui/react'
 import React from 'react'
-
+import {RotatingLines} from 'react-loader-spinner'
 const Loader = () => {
   return (
   <VStack h="100vh" justifyContent={"center"}  >
 <div style={{transform:"scale(3)"}}>
-
+<RotatingLines
+  strokeColor="#4873da"
+  strokeWidth="5"
+  animationDuration="0.75"
+  width="70"
+  visible={true}
+/>
 </div>
     </VStack>
   )
